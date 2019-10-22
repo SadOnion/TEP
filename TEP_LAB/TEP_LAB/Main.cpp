b093b06b46ecaddee1a7ade50e28144c2a57c608
SadOnion <30446684+SadOnion@users.noreply.github.com>
2019-10-22 12:11:26 +0200

    Cleaned up ex1 functions

--- TEP_LAB/TEP_LAB/Main.cpp
+++ TEP_LAB/TEP_LAB/Main.cpp
@@ -3,115 +3,6 @@
 #include "Utils.h"
 
 
-
-int* AllocTableAdd5(int iSize) {
-
-	if (iSize <= 0) {
-		return 0;
-	}
-	const int OFFSET_PLUS = 5;
-
-	int* pi_table;
-	pi_table = new int[iSize];
-
-	for (int i = 0; i < iSize; i++)
-	{
-		pi_table[i] = i + OFFSET_PLUS;
-	}
-
-	return pi_table;
-}
-
-
-bool AllocTable2Dim(int*** piTable, int iSizeX, int iSizeY) {
-	if (iSizeX <= 0 || iSizeY < 0) return false;
-
-	*piTable = new int* [iSizeX];
-
-	for (int i = 0; i < iSizeX; i++)
-	{
-		(*piTable)[i] = new int[iSizeY];
-	}
-
-
-	return true;
-}
-
-bool DeallocTable2Dim(int** piTable, int iSizeX) {
-
-	if (iSizeX <= 0)return false;
-
-	for (int i = 0; i < iSizeX; i++)
-	{
-		delete[] piTable[i];
-	}
-
-	delete piTable;
-	return true;
-}
-void Show2DimTable(int** table, int xSize, int ySize) {
-
-	for (int i = 0; i < xSize; i++)
-	{
-		for (int j = 0; j < ySize; j++)
-		{
-			std::cout << table[i][j] << "; ";
-		}
-		std::cout << std::endl;
-	}
-	std::cout << std::endl;
-}
-
-void Fill2DimTable(int** table, int xSize, int ySize, int value) {
-
-	for (int i = 0; i < xSize; i++)
-	{
-		for (int j = 0; j < ySize; j++)
-		{
-			table[i][j] = value;
-		}
-
-	}
-}
-
-void FillBorderTable2Dim(int** table, int xSize, int ySize, int value) {
-
-	for (int i = 0; i < ySize; i++)
-	{
-		table[0][i] = value;
-		table[xSize - 1][i] = value;
-	}
-	for (int i = 0; i < xSize; i++)
-	{
-		table[i][0] = value;
-		table[i][ySize - 1] = value;
-	}
-}
-void List1() {
-
-	int size = 5;
-
-	int* tablePtr = AllocTableAdd5(size);
-	if (tablePtr != 0) {
-		PrintTable(tablePtr, size);
-		delete tablePtr;
-	}
-	int** pi_table;
-
-	int xSize = 10;
-	int ySize = 4;
-
-	if (AllocTable2Dim(&pi_table, xSize, ySize)) {
-
-		Fill2DimTable(pi_table, xSize, ySize, 10);
-		Show2DimTable(pi_table, xSize, ySize);
-		FillBorderTable2Dim(pi_table, xSize, ySize, 99);
-		Show2DimTable(pi_table, xSize, ySize);
-		DeallocTable2Dim(pi_table, xSize);
-
-	}
-}
-
 void ModifyTable(CTable* tab, int newSize) {
 	tab->SetNewSize(newSize);
 }
@@ -156,6 +47,9 @@ void List3() {
 
 }
 
+void List4() {
+
+}
 int main()
 {
 	List3();
